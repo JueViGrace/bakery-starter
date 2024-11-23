307a0238bc6bd5274ce593f8c0b9176b16e13786
Daniel Zabala <danielzabalanava@gmail.com>
2024-11-23 15:03:26 -0400

    auth subprojects for server and client

---
+++ lib/auth/domain/shared/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+}
+
+kotlin {
+    androidTarget {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+        }
+    }
+
+    jvm()
+
+    sourceSets {
+        androidMain.dependencies {
+        }
+
+        commonMain.dependencies {
+            // Coroutines
+            implementation(libs.kotlinx.coroutines.core)
+        }
+
+        jvmMain.dependencies {
+            // Coroutines
+            implementation(libs.kotlinx.coroutines.swing)
+        }
+    }
+}
+
+android {
+    namespace = "com.bakery.auth.domain.shared"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
+
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
